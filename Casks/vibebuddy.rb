950accd6eaf10e9ad46d7596a93447892ac73ad0
Cyril Pereira <cyril.pereira@gmail.com>
2026-08-30 17:36:03 +0200

    bump v1.0.1

--- Casks/vibebuddy.rb
+++ Casks/vibebuddy.rb
@@ -1,6 +1,6 @@
 cask "vibebuddy" do
-  version "1.0.0"
-  sha256 "fbc26aafc95e8cc9e06ed671d73bf0906796d546818edcff4255afd55ce6e69c"
+  version "1.0.1"
+  sha256 "146e7e11d9ca3fa6f5f1b4bc9dbf88fcd3860ab67ec269e28d6ecbcdd4b21895"
 
   url "https://github.com/funkymed/VibeBuddy/releases/download/v#{version}/VibeBuddy-#{version}.dmg"
   name "VibeBuddy"
